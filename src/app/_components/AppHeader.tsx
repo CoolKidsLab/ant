dd76ae166e6360880f1b7f61bfd9f3da931d23c0
Yang Zhao <jenson.zhao@gmail.com>
2025-03-29 08:06:12 -0700

    feat: enhance AppHeader with logo and updated styling

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -2,11 +2,16 @@ export function AppHeader() {
   return (
     <div>
       <a
-        className="font-serif text-lg font-extralight text-gray-500"
+        className="font-serif text-2xl font-extralight text-gray-500"
         href="https://github.com/CoolKidsLabs/ant"
         target="_blank"
       >
-        Ant
+        <div className="flex items-end space-x-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+            <text y="28" font-size="28" text-anchor="middle" x="16">▞</text>
+          </svg>
+          <p className="leading-none">Ant</p>
+        </div>
       </a>
     </div>
   );
