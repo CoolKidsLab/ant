26fe088446b55b019794df412740c530cf0f2de0
Yang Zhao <jenson.zhao@gmail.com>
2025-03-30 10:01:51 -0700

    fix: correct SVG text attributes in AppHeader component

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -8,7 +8,7 @@ export function AppHeader() {
       >
         <div className="flex items-end space-x-2">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-            <text y="28" font-size="28" text-anchor="middle" x="16">▞</text>
+            <text y="28" fontSize="28" textAnchor="middle" x="16">▞</text>
           </svg>
           <p className="leading-none">Ant</p>
         </div>
